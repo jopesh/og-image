8882061245970df700f4e16875ef994f2e4a5e66
John Schmidt <mail@johnschmidt.de>
2020-07-24 23:47:03 +0200

    new logo

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -1,73 +1,73 @@
-import { IncomingMessage } from "http";
-import { parse } from "url";
-import { ParsedRequest, Theme } from "./types";
+import { IncomingMessage } from 'http'
+import { parse } from 'url'
+import { ParsedRequest, Theme } from './types'
 
-export function parseRequest(req: IncomingMessage) {
-  console.log("HTTP " + req.url);
-  const { pathname, query } = parse(req.url || "/", true);
-  const { fontSize, images, widths, heights, theme, md } = query || {};
+export function parseRequest (req: IncomingMessage) {
+  console.log('HTTP ' + req.url)
+  const { pathname, query } = parse(req.url || '/', true)
+  const { fontSize, images, widths, heights, theme, md } = query || {}
 
   if (Array.isArray(fontSize)) {
-    throw new Error("Expected a single fontSize");
+    throw new Error('Expected a single fontSize')
   }
   if (Array.isArray(theme)) {
-    throw new Error("Expected a single theme");
+    throw new Error('Expected a single theme')
   }
 
-  const arr = (pathname || "/").slice(1).split(".");
-  let extension = "";
-  let text = "";
+  const arr = (pathname || '/').slice(1).split('.')
+  let extension = ''
+  let text = ''
   if (arr.length === 0) {
-    text = "";
+    text = ''
   } else if (arr.length === 1) {
-    text = arr[0];
+    text = arr[0]
   } else {
-    extension = arr.pop() as string;
-    text = arr.join(".");
+    extension = arr.pop() as string
+    text = arr.join('.')
   }
 
   const parsedRequest: ParsedRequest = {
-    fileType: extension === "jpeg" ? extension : "png",
+    fileType: extension === 'jpeg' ? extension : 'png',
     text: decodeURIComponent(text),
-    theme: theme === "dark" ? "dark" : "light",
-    md: md === "1" || md === "true",
-    fontSize: fontSize || "96px",
+    theme: theme === 'dark' ? 'dark' : 'light',
+    md: md === '1' || md === 'true',
+    fontSize: fontSize || '96px',
     images: getArray(images),
     widths: getArray(widths),
-    heights: getArray(heights),
-  };
+    heights: getArray(heights)
+  }
   parsedRequest.images = getDefaultImages(
     parsedRequest.images,
     parsedRequest.theme
-  );
-  return parsedRequest;
+  )
+  return parsedRequest
 }
 
-function getArray(stringOrArray: string[] | string | undefined): string[] {
-  if (typeof stringOrArray === "undefined") {
-    return [];
+function getArray (stringOrArray: string[] | string | undefined): string[] {
+  if (typeof stringOrArray === 'undefined') {
+    return []
   } else if (Array.isArray(stringOrArray)) {
-    return stringOrArray;
+    return stringOrArray
   } else {
-    return [stringOrArray];
+    return [stringOrArray]
   }
 }
 
-function getDefaultImages(images: string[], theme: Theme): string[] {
+function getDefaultImages (images: string[], theme: Theme): string[] {
   const defaultImage =
-    theme === "light"
-      ? "https://www.datocms-assets.com/28171/1592162569-js-black.svg"
-      : "https://www.datocms-assets.com/28171/1592162570-js-white.svg";
+    theme === 'light'
+      ? 'https://www.datocms-assets.com/31429/1595627039-jopesh.svg'
+      : 'https://www.datocms-assets.com/28171/1592162570-js-white.svg'
 
   if (!images || !images[0]) {
-    return [defaultImage];
+    return [defaultImage]
   }
   if (
-    !images[0].startsWith("https://assets.vercel.com/") &&
-    !images[0].startsWith("https://assets.zeit.co/") &&
-    !images[0].startsWith("https://www.datocms-assets.com/")
+    !images[0].startsWith('https://assets.vercel.com/') &&
+    !images[0].startsWith('https://assets.zeit.co/') &&
+    !images[0].startsWith('https://www.datocms-assets.com/')
   ) {
-    images[0] = defaultImage;
+    images[0] = defaultImage
   }
-  return images;
+  return images
 }
